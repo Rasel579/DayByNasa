cd948d91e006126c15e0d8efe51df664b92e9b92
Rasel579 <Rasel579@gmail.com>
2021-07-25 16:50:00 +0500

    added splash screen, change UI/US

--- app/src/main/java/com/app_maker/models/rest/BackendApi.kt
+++ app/src/main/java/com/app_maker/models/rest/BackendApi.kt
@@ -6,7 +6,8 @@ import retrofit2.http.Query
 
 interface BackendApi {
    @GET("planetary/apod")
-   fun getDataFromNasa(@Query("api_key") apiKey: String) : Call<NasaPictureDTO>@GET("planetary/apod")
+   fun getDataFromNasa(@Query("api_key") apiKey: String) : Call<NasaPictureDTO>
+   @GET("planetary/apod")
    fun getDataFromNasaPreviousDate(@Query("date") date : String, @Query("api_key") apiKey: String) : Call<NasaPictureDTO>
    @GET("EPIC/api/natural/date/")
    fun getDataFromNasaEpicApi(@Query("date") date: String, @Query("api_key") apiKey: String): Call<MutableList<NasaEpicDTO>>
